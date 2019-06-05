5fa6f3b33d79541cac069282b8e4a56db077da35
nboutin <boutwork@gmail.com>
2019-06-05 09:27:50 +0200

    add hide and reveal script

---
+++ hide.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -lt 1 ] 
+then
+	echo "Error: missing password argument"
+	exit
+fi
+
+dest=archive.7z
+
+rm $dest
+
+7z -mhc=on -mhe=on -p"$1" a $dest gnucash_nboutin
+7z -mhc=on -mhe=on -p"$1" a $dest template
---
+++ reveal.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+src=archive.7z
+
+7z x $src
